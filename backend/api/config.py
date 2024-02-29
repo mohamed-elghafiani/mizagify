d108228bf6b0d1dfd8a6cedeea23fc256dbb3080
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-02-29 13:55:03 +0100

    refactoring: moved config to api/

--- backend/api/config.py
+++ backend/api/config.py
@@ -10,18 +10,22 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_SECRET_KEY = config("JWT_SECRET_KEY", )
+    CORS_HEADERS = "Content-Type"
 
 
 class DevConfig(Config):
     """Development Configuration class"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 class TestConfig(Config):
     """Test Configuration class"""
-    pass
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_ECHO = True
 
 class ProdConfig(Config):
     """Test Configuration class"""
